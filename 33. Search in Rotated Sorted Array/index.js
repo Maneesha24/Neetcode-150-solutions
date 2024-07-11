51b81c422e71074e55f4137898df9f912e5971e3
Maneesha Venigalla <maneeshavenigalla@maneesh63211fg4.mynetworksettings.com>
2024-07-10 21:14:30 -0400

    Fixed 33

--- 33. Search in Rotated Sorted Array/index.js
+++ 33. Search in Rotated Sorted Array/index.js
@@ -11,24 +11,23 @@ const search = (nums, target) => {
   let left = 0;
   let right = nums.length - 1;
 
-  while (left < right) {
+  while (left <= right) {
     let mid = Math.floor((left + right) / 2);
-
     if (nums[mid] === target) {
       return mid;
     }
 
-    if (nums[mid] >= nums[right]) {
-      if (nums[mid] >= nums[right] && target < nums[mid]) {
-        left = mid + 1;
-      } else {
+    if (nums[mid] >= nums[left]) {
+      if (nums[left] <= target && target <= nums[mid]) {
         right = mid - 1;
+      } else {
+        left = mid + 1;
       }
     } else {
-      if (nums[mid] >= nums[left] && nums[mid] > target) {
-        right = mid - 1;
-      } else {
+      if (nums[mid] <= target && target <= nums[right]) {
         left = mid + 1;
+      } else {
+        right = mid - 1;
       }
     }
   }
